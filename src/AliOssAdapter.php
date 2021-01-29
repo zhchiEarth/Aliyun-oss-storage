9e57299e2688ab764a45b4f90d441284b27c4767
zhchiEarth <zhchi_earth@163.com>
2021-01-29 10:55:48 +0800

    提交

--- src/AliOssAdapter.php
+++ src/AliOssAdapter.php
@@ -568,7 +568,11 @@ class AliOssAdapter extends AbstractAdapter
      */
     public function getUrl( $path )
     {
-        if (!$this->has($path)) throw new FileNotFoundException($filePath.' not found');
+        if (!$this->has($path)) throw new FileNotFoundException($path.' not found');
+        
+        if ($this->cdnDomain) {
+            return ( $this->ssl ? 'https://' : 'http://' ) . $this->cdnDomain. '/'. ltrim($path, '/');
+        }
         return ( $this->ssl ? 'https://' : 'http://' ) . ( $this->isCname ? ( $this->cdnDomain == '' ? $this->endPoint : $this->cdnDomain ) : $this->bucket . '.' . $this->endPoint ) . '/' . ltrim($path, '/');
     }
 
